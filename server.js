d64f6a284e1b13cde9113f4213901f569e197594
SaiChandra2896 <saichandra2896@gmail.com>
2020-09-11 14:56:35 +0530

    add event type and event input types and start querying for them

--- server.js
+++ server.js
@@ -1,8 +1,63 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); 
+const graphqlHttp = require('express-graphql');
+// to build graphql schema
+const { buildSchema }= require('graphql');
 
 const app = express();
 app.use(bodyParser.json());
+
+const events = [];
+
+// use graphql middleware
+app.use('/graphql', graphqlHttp.graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    // we write resolvers in rootValue
+    rootValue:{
+        events: () =>{
+            return events;
+        },
+        createEvent: (args) =>{
+            const {title, description, price} = args.eventInput;
+            const event = {
+                _id: Math.random().toString(),
+                title, description, price: +price,
+                date: new Date().toISOString()
+            }
+            events.push(event);
+            return event;
+        }
+    },
+    graphiql: true
+}));
 
 const PORT = process.env.PORT || 3000;
 
