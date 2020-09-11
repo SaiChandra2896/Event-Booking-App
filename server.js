6be925b6aacfc7b67a3b44ad512032fe5bba44d2
SaiChandra2896 <saichandra2896@gmail.com>
2020-09-11 13:39:47 +0530

    server up

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is up on port ${PORT}`));
