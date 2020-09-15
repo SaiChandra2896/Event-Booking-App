89ae62af648dc5611b75a309e97afbbf53d3175e
SaiChandra2896 <saichandra2896@gmail.com>
2020-09-15 18:31:52 +0530

    add backdrop for modal

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -10,8 +10,8 @@ const Modal = (props) => {
                 {props.children}
             </section>
             <section className="modal__actions">
-                {props.canCancel && <button className="btn">Cancel</button>}
-                {props.canConfirm && <button className="btn">Confirm</button>}
+                {props.canCancel && <button className="btn" onClick={props.onCancel}>Cancel</button>}
+                {props.canConfirm && <button className="btn" onClick={props.onConfirm}>Confirm</button>}
             </section>
         </div>
     )
--- frontend/src/pages/Events/EventsPage.js
+++ frontend/src/pages/Events/EventsPage.js
@@ -1,18 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Modal from '../../components/Modal/Modal';
+import Backdrop from '../../components/Backdrop/Backdrop';
 
 import './Events.css';
 
 const EventsPage = () => {
+    const [creating,setCreating] = useState(false);
+    
+    const createEvenHandler = () =>{
+        setCreating(true);
+    }
+
+    const modalCofirmhandler = () =>{
+        setCreating(false);
+    };
+
+    const modalCancelHandler = () =>{
+        setCreating(false)
+    }
+
     return (
         <Fragment>
-            <Modal title="Add Event" canCancel canConfirm>
+            {creating && <Backdrop/>}
+            {creating && <Modal title="Add Event" canCancel canConfirm onCancel={modalCancelHandler} onConfirm={modalCofirmhandler}>
                 <p>Modal Content</p>
-            </Modal>
+                        </Modal>}
             <div className="events-control">
                 <p>Share Your Events!!</p>
-                <button className="btn">Create Event</button>
+                <button className="btn" onClick={createEvenHandler}>Create Event</button>
             </div>
         </Fragment>
     )
