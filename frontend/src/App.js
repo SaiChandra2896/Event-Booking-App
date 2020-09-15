0816ca5ebf88ec0a4d518d00ea90b65ea7c9f396
SaiChandra2896 <saichandra2896@gmail.com>
2020-09-15 11:19:15 +0530

    Add navbar along with css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import React,{useState} from 'react';
+import React,{Fragment, useState} from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+
+import Navigation from './components/Navigation/Navigation';
 
 import AuthPage from './pages/AuthPage';
 import BookingsPage from  './pages/BookingsPage';
@@ -7,15 +9,20 @@ import EventsPage from './pages/EventsPage';
 
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Switch>
-      <Redirect from="/" to="/auth" exact/>
-      <Route path="/auth" component={AuthPage}/>
-      <Route path="/events" component={EventsPage}/>
-      <Route path="/bookings" component={BookingsPage}/>
-      </Switch>
+      <Fragment>
+        <Navigation/>
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/auth" exact/>
+            <Route path="/auth" component={AuthPage}/>
+            <Route path="/events" component={EventsPage}/>
+            <Route path="/bookings" component={BookingsPage}/>
+          </Switch>
+        </main>
+      </Fragment>
     </Router>
   );
 }
---
+++ frontend/src/components/Navigation/Navigation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+import './Navigation.css';
+
+const Navigation = (props) => {
+    return (
+        <header className="main-navigation">
+           <div className="main-navigation__logo">
+               <h1>EasyEvent</h1>
+           </div>
+           <nav className="main-navigation__items">
+                <ul>
+                    <li>
+                        <NavLink to="/auth">
+                            Authentication
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/events">
+                            Events
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/bookings">
+                            Bookings
+                        </NavLink>
+                    </li>
+                </ul>
+           </nav>
+        </header>
+    )
+}
+
+export default Navigation;
